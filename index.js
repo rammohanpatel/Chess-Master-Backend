2ca34c949df96ded97b750d715a63bfe87df5f0e
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2024-09-24 18:44:08 +0530

    latest

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ const http = require('http');
 const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
-const cors = require('cors');
 const io = new Server(server, {
   cors: {
-    origin: ['https://chess-master-frontend.vercel.app', 'http://localhost:3000'], // Allow frontend and localhost
+    origin: '*', // Allow frontend and localhost
     methods: ['GET', 'POST'],
   },
 });
