f8b2d2820ede9fe21f6773baa837f171f2caea1c
Ram Mohan Patel <103207998+rammohanpatel@users.noreply.github.com>
2024-09-24 17:56:36 +0530

    latest

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // Allow Next.js frontend
+    origin: 'https://chess-master-frontend.vercel.app/', // Allow Next.js frontend
     methods: ['GET', 'POST'],
   },
 });
